c20ed35069972d812fd1e07a1c4ad8a3b2bed5c9
Saurav Gharti <sp.gharti@gmail.com>
2021-03-22 14:17:40 +0545

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import CustomText
 
-admin.site.register(CustomText)
+@admin.register(CustomText)
+class CusomTextAdmin(admin.ModelAdmin):
+  pass
 
 # Register your models here.
