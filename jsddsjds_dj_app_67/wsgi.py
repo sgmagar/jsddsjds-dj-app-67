46de2a273a1d759b95e3d3160db28360c559454f
Saurav <sp.gharti@gmail.com>
2021-03-22 08:30:44 +0000

    Update models

--- jsddsjds_dj_app_67/wsgi.py
+++ jsddsjds_dj_app_67/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsddsjds_dj_app_67.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jsddsjds_dj_app_67.settings")
 
 application = get_wsgi_application()
