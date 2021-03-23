d133c6bc64c93bec74dd17f0ec97fab909c42a80
Saurav <sp.gharti@gmail.com>
2021-03-23 06:40:22 +0000

    Update models

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
     UserDetailView,
-    user_redirect_view,
     user_detail_view,
-    UserUpdateView,
     user_update_view,
+    user_redirect_view,
+    UserUpdateView,
 )
 
 app_name = "users"
